85043d22bedd51e27e6f3ffb5acaa85dec18e571
olgakaras <olga.karas123@gmail.com>
2021-10-16 00:10:54 +0300

    feat: add metods in has and remove and root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,15 +1,79 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+function removeNode (node, key){
+  if(node === null)
+      return null;
+  else if(key < node.data)
+  {
+      node.left = this.removeNode(node.left, key);
+      return node;
+  }
+  else if(key > node.data)
+  {
+      node.right = this.removeNode(node.right, key);
+      return node;
+  }
+  else
+    {
+      // deleting node with no children
+      if(node.left === null && node.right === null)
+      {
+          node = null;
+          return node;
+      }
+
+      // deleting node with one children
+      if(node.left === null)
+      {
+          node = node.right;
+          return node;
+      }
+        
+      else if(node.right === null)
+      {
+          node = node.left;
+          return node;
+      }
+      var aux = this.findMinNode(node.right);
+      node.data = aux.data;
+
+      node.right = this.removeNode(node.right, aux.data);
+      return node;
+    }
+}
+
+function hasNode (node, data){
+  if(node === null) return false;
+
+  else if(data < node.data){
+    if(node.left == null) return false;
+    else return this.hasNode(node.left, data);
+  }
+
+  else if(data > node.data){
+    if(node.right == null) return false;
+    else return this.hasNode(node.right, data);
+  }
+    
+  else return true;
+}
+
 module.exports = class BinarySearchTree {
+  constructor() {
+    this.headTree = null;
+  }
+
 
   root() {
-    throw new NotImplementedError('Not implemented');
+    return this.headTree;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
@@ -18,8 +82,11 @@ module.exports = class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    if (data == true) return true;
+    else return  hasNode(this.headTree, data);
+    //this.headTree = this.hasNode(this.headTree, data);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
@@ -28,8 +95,9 @@ module.exports = class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    this.headTree = this.removeNode(this.headTree, data);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
